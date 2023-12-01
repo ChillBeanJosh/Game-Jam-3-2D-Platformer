ce1445325a729b73d7ac177f4b511d458be49a6e
ChillBeanJosh <joshover23@gmail.com>
2023-12-01 02:46:02 -0500

    levels complete

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
  
 }
